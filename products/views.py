c990cc9da7db7e274a272f81ae1fc26519d8555a
omar <omar.gamaleldin19@gmail.com>
2025-08-20 15:30:07 +0300

    Removed unnecessary url and view'

--- products/views.py
+++ products/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404,redirect
 from products.models import techProducts
-
 # Create your views here.
-
 def productspage(request):
     return render(request,'index.html')
 
@@ -13,15 +11,6 @@ def landingpage(request):
 
 def test_layout(request):
     return render(request,'layouts/base.html')
-
-# def test_file(request):
-#     products = [
-#          {"name": "Dell Laptop", "price": 2000, "category": "Laptops"},
-#         {"name": "Samsung Phone", "price": 1000, "category": "Phones"},
-#         {"name": "Dark Souls", "price": 500, "category": "Games"},
-#         {"name": "Hp Laptop", "price": 800, "category": "Laptops"},
-#     ]
-#     return render(request,'test.html',context={'products':products})
 
 def all_products(request):
     products = techProducts.objects.all()
@@ -39,8 +28,6 @@ def delete_product(request,product_id):
 def create_product(request):
     print(request)
     if request.method == 'POST':
-
-
         pname = request.POST.get('name')
         pprice = request.POST.get('price')
         pinstock = request.POST.get('instock')
